fefa1ea39271a4337c6edcd26e8895728d4e86bc
Galina Shchepotina <113752322+galsepo@users.noreply.github.com>
2022-10-20 15:51:17 -0700

    Update style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     const [toggle, setToggle] = useState(false)
     return (
         <nav className='w-full flex py-6 justify-between items-center navbar'>
-            <img src={logo} alt="Hoobank" className='w-[124px] h-[32px]' />
+            <img src={logo} alt="Hoobank" className='w-[300px] h-[90px]' />
             <ul className='list-none sm:flex hidden justify-end items-center flex-1'>{navLinks.map((nav, index) => (
                 <li
                     key={nav.id}
